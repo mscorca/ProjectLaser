3fbe222df2b3b2f88791eab7d441288838c14f7d
Matt Scorca <mscorca@ucsc.edu>
2016-01-12 20:16:11 -0800

    initial commit. Two little dudes controlled with input manager axes

---
+++ Assets/Scripts/Player1Controller.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player1Controller : MonoBehaviour {
+	public float speed = 1;
+	public int playerNum;
+
+
+	public void Update(){
+		Move();
+	}
+
+	public void Move() 
+	{
+        float transVert = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float transHoriz = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        transform.Translate(transHoriz, transVert, 0);
+	}
+}
---
+++ Assets/Scripts/Player2Controller.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player2Controller : MonoBehaviour {
+	public float speed = 1;
+	public int playerNum;
+
+
+	public void Update(){
+		Move();
+	}
+
+	public void Move() 
+	{
+        float transVert = Input.GetAxis("Vertical2") * speed * Time.deltaTime;
+        float transHoriz = Input.GetAxis("Horizontal2") * speed * Time.deltaTime;
+        transform.Translate(transHoriz, transVert, 0);
+	}
+}

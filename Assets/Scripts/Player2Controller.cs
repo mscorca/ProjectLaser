f1fa5f9186032c246517bf4ba4979af81b6f0741
Matt <mattscorca@gmail.com>
2016-01-19 19:38:45 -0800

    Should work with ps4 controller now

--- Assets/Scripts/Player2Controller.cs
+++ Assets/Scripts/Player2Controller.cs
@@ -12,8 +12,17 @@ public class Player2Controller : MonoBehaviour {
 
 	public void Move() 
 	{
-        float transVert = Input.GetAxis("Vertical2") * speed * Time.deltaTime;
-        float transHoriz = Input.GetAxis("Horizontal2") * speed * Time.deltaTime;
-        transform.Translate(transHoriz, transVert, 0);
+		float transVert = 0;
+		float transHoriz = 0;
+
+		Debug.Log("Vertical: " + Input.GetAxis("Vertical2"));
+		Debug.Log("Horizontal: " + Input.GetAxis("Horizontal2"));
+
+        if(Mathf.Abs(Input.GetAxis("Vertical2")) > 0.01 ){
+        	transVert = Input.GetAxis("Vertical2") * speed * Time.deltaTime;
+        }
+        transHoriz = Input.GetAxis("Horizontal2") * speed * Time.deltaTime;
+       	
+       	transform.Translate(transHoriz, transVert, 0);		
 	}
 }
